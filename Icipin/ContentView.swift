a5525c86c6f528022b4de1c23f862875c35f2cc8
rivaldofez <rivaldofez@gmail.com>
2022-08-15 09:36:35 +0700

    Initialization Projects

---
+++ Icipin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Icipin
+//
+//  Created by Rivaldo Fernandes on 15/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Icipin/IcipinApp.swift
@@ -0,0 +1,17 @@
+//
+//  IcipinApp.swift
+//  Icipin
+//
+//  Created by Rivaldo Fernandes on 15/08/22.
+//
+
+import SwiftUI
+
+@main
+struct IcipinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
