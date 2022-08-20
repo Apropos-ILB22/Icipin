5cad8a0a8e046c354b55f24eefd6cc5512269a35
rivaldofez <rivaldofez@gmail.com>
2022-08-21 02:42:11 +0700

    update core data manager

--- Icipin/CoreData/CoreDataManager.swift
+++ Icipin/CoreData/CoreDataManager.swift
@@ -8,4 +8,31 @@
 import SwiftUI
 import CoreData
 
-
+class CoreDataManager {
+    let persistentContainer: NSPersistentContainer
+    
+    static let shared = CoreDataManager() //singleton creational pattern
+    
+    var viewContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    } //getter for context
+    
+    private init() {
+        persistentContainer = NSPersistentContainer(name: "Icipin")
+        persistentContainer.loadPersistentStores{(description, error) in
+            if let error = error {
+                fatalError("Cannot initialize Core Data because \(error)")
+            }
+        }
+    }
+    
+    func saveContext(){
+        do {
+            try viewContext.save()
+        } catch {
+            viewContext.rollback()
+            print(error.localizedDescription)
+        }
+    }
+    
+}
