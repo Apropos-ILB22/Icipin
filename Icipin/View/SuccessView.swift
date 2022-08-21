1014f26a556ffe771f77b716ef55ded4639476af
rivaldofez <rivaldofez@gmail.com>
2022-08-21 16:02:46 +0700

    update component success view

--- Icipin/View/SuccessView.swift
+++ Icipin/View/SuccessView.swift
@@ -13,15 +13,26 @@ struct SuccessView: View {
             
             Spacer()
             
-            Text("Selamat! Kamu Berhasil membuka quest Bola Gurita")
-                .font(.title2).bold()
-            Image("sandwich")
-            
+            Text("Selamat! Kamu Berhasil membuka quest BOLA GURITA")
+                .font(.title2).fontWeight(.semibold)
+                .foregroundColor(Color("textcolor"))
+                .multilineTextAlignment(.center)
             Spacer()
+            Image("sandwich")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: UIScreen.main.bounds.width * 0.6, height: UIScreen.main.bounds.height * 0.3)
             
+
             Text("TAKOYAKI")
+                .font(.title).bold()
+                .foregroundColor(Color("textcolor"))
             
             Text("Tekan gambar untuk melihat informasi \n menarik tentang Takoyaki")
+                .font(.body)
+                .foregroundColor(Color("textcolor"))
+                .multilineTextAlignment(.center)
+                .padding(.top, 20)
             
             Spacer()
             
