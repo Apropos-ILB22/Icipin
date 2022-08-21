8b80d0e6bbc61f68bb6639865e2f6a1886cf21fc
rivaldofez <rivaldofez@gmail.com>
2022-08-21 16:31:09 +0700

    update size button

--- Icipin/View/SuccessView.swift
+++ Icipin/View/SuccessView.swift
@@ -22,22 +22,22 @@ struct SuccessView: View {
             Spacer()
             ZStack{
                 
-                Circle().fill(Color("casper").opacity(0.1)).frame(width: UIScreen.main.bounds.width * 0.5 + 250, height: UIScreen.main.bounds.width * 0.5 + 250)
+                Circle().fill(Color("casper").opacity(0.1)).frame(width: UIScreen.main.bounds.width * 0.4 + 250, height: UIScreen.main.bounds.width * 0.4 + 250)
                     .scaleEffect(self.animate ? 1 : 0)
                 
-                Circle().fill(Color("casper").opacity(0.7)).frame(width: UIScreen.main.bounds.width * 0.5 + 200, height: UIScreen.main.bounds.width * 0.5 + 200)
+                Circle().fill(Color("casper").opacity(0.7)).frame(width: UIScreen.main.bounds.width * 0.4 + 200, height: UIScreen.main.bounds.width * 0.4 + 200)
                     .scaleEffect(self.animate ? 1 : 0)
                 
-                Circle().fill(Color("geyser")).frame(width: UIScreen.main.bounds.width * 0.5 + 150, height: UIScreen.main.bounds.width * 0.5 + 150)
+                Circle().fill(Color("geyser")).frame(width: UIScreen.main.bounds.width * 0.4 + 150, height: UIScreen.main.bounds.width * 0.4 + 150)
                     .scaleEffect(self.animate ? 1 : 0)
                 
-                Circle().fill(Color("seashell")).frame(width: UIScreen.main.bounds.width * 0.5 + 100, height: UIScreen.main.bounds.width * 0.5 + 100)
+                Circle().fill(Color("seashell")).frame(width: UIScreen.main.bounds.width * 0.4 + 100, height: UIScreen.main.bounds.width * 0.4 + 100)
                     .scaleEffect(self.animate ? 1 : 0)
                 
                 Image("sandwich")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: UIScreen.main.bounds.width * 0.5, height: UIScreen.main.bounds.height * 0.3)
+                    .frame(width: UIScreen.main.bounds.width * 0.4, height: UIScreen.main.bounds.height * 0.3)
                     .padding(UIScreen.main.bounds.width * 0.1)
                     .background(Circle().foregroundColor(.white))
             }.onAppear{
