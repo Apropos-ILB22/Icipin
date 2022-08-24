ef7fb5707a5c7ce93738c63505627ac239365c8a
rivaldofez <rivaldofez@gmail.com>
2022-08-24 21:43:13 +0700

    add model json and update format json

---
+++ Icipin/Model/Model.swift
@@ -0,0 +1,41 @@
+//
+//  Model.swift
+//  Icipin
+//
+//  Created by Rivaldo Fernandes on 24/08/22.
+//
+
+import SwiftUI
+
+// MARK: - User
+struct UserModel: Codable {
+    let name: String
+    let quest: [QuestModel]
+}
+
+// MARK: - Quest
+struct QuestModel: Codable {
+    let category, foodName, hexcolor, icon: String
+    let status: Int
+    let story, title: String
+    let clue: [ClueModel]
+    let places: [PlaceModel]
+
+    enum CodingKeys: String, CodingKey {
+        case category
+        case foodName = "food_name"
+        case hexcolor, icon, status, story, title, clue, places
+    }
+}
+
+// MARK: - Clue
+struct ClueModel: Codable {
+    let title: String
+}
+
+// MARK: - Place
+struct PlaceModel: Codable {
+    let name: String
+    let latitude, longitude, rating: Double
+}
+
